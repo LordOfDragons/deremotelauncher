a809390e37b36515eb7b65f17b8b9b09c2717dff
Plüss Roland <roland@rptd.ch>
2024-12-04 22:45:33 +0100

    modified SDK structure

--- vs/staticLibrary/post_build.ps1
+++ vs/staticLibrary/post_build.ps1
@@ -7,7 +7,7 @@
 Import-Module "$PSScriptRoot\..\shared.psm1"
 
 # headers
-$TargetDir = "$OutputDir\SDK\Include\deremotelauncher"
+$TargetDir = "$OutputDir\DERemoteLauncherSDK\Include\deremotelauncher"
 if (Test-Path $TargetDir) {
     Remove-Item $TargetDir -Force -Recurse
 }
@@ -17,17 +17,17 @@ Copy-Files -SourceDir $SourceDir -TargetDir $TargetDir -Pattern "*.h"
 Copy-Item -Path "$ProjectDir\config.h" -Destination $TargetDir
 
 # library
-$TargetDir = "$OutputDir\SDK\Library\x64"
+$TargetDir = "$OutputDir\DERemoteLauncherSDK\Library\x64"
 Write-Host "DERemoteLauncher: Copy Library to '$TargetDir'"
 Install-Files -Path "$OutputDir\DERemoteLauncher.lib" -Destination $TargetDir
 
 # pdb
-$TargetDir = "$OutputDir\SDK\Library\x64"
+$TargetDir = "$OutputDir\DERemoteLauncherSDK\Library\x64"
 Write-Host "DERemoteLauncher: Copy PDB to '$TargetDir'"
 Install-Files -Path "$OutputDir\DERemoteLauncher.pdb" -Destination $TargetDir
 
 # info
-$TargetDir = "$OutputDir\SDK"
+$TargetDir = "$OutputDir\DERemoteLauncherSDK"
 Write-Host "DERemoteLauncher: Copy Info to '$TargetDir'"
 Copy-Item -Path "$SourceDir\..\..\LICENSE" -Destination $TargetDir
 Copy-Item -Path "$ProjectDir\..\..\README.md" -Destination $TargetDir
